0efe1623184e82d98bedc29a690b428b21123966
pablo <pablovicenteroura2005@gmail.com>
2024-11-08 17:53:33 +0100

    commit

--- src/main/java/com/client/ControllerClients.java
+++ src/main/java/com/client/ControllerClients.java
@@ -2,10 +2,13 @@ package com.client;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,6 +24,9 @@ public class ControllerClients implements Initializable {
     private Button addUsersButton;
 
     @FXML
+    private Button logInButton;
+
+    @FXML
     private GridPane rootGridPane;
 
     @FXML
@@ -34,12 +40,27 @@ public class ControllerClients implements Initializable {
     }
 
     @FXML
+    private void logInUser(ActionEvent event) {
+        System.out.println("Se pulsó el botón iniciarSesión");
+    }
+
+    @FXML
     private void addUsersButton(ActionEvent event) {
         System.out.println("Se pulsó el botón addUsers");
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        try {
+
+            Parent loadedPane = FXMLLoader.load(getClass().getResource("/assets/layout_currentClients.fxml"));
+            GridPane.setColumnIndex(loadedPane, 1);
+            GridPane.setRowIndex(loadedPane, 0);
+            rootGridPane.getChildren().add(loadedPane);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("Cargó");
     }
 }
