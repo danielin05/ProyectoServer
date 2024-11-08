8e392a8fd08ea2871106759d4920371777b90b55
pablo <pablovicenteroura2005@gmail.com>
2024-11-08 18:41:45 +0100

    cargar lista de productos

--- src/main/java/com/server/Main.java
+++ src/main/java/com/server/Main.java
@@ -6,15 +6,23 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.jline.reader.EndOfFileException;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.UserInterruptException;
 
+import java.io.File;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Main extends WebSocketServer {
 
@@ -22,11 +30,16 @@ public class Main extends WebSocketServer {
     private List<ClientFX> currentClients;
     private List<ClientFX> clients;
 
+    private static JSONArray productList;
+
     public Main(InetSocketAddress address) {
         super(address);
         webSockets = new ArrayList<>();
         currentClients = new ArrayList<>();
         clients = new ArrayList<>();
+
+        this.productList = loadProducts();
+
         clients.add(new ClientFX("Admin", 1, 1, null));
         clients.add(new ClientFX("Responsable", 2, 2, null));
         clients.add(new ClientFX("Cliente", 3, 3, null));
@@ -103,6 +116,8 @@ public class Main extends WebSocketServer {
                     System.out.println("Stopping server...");
                     server.stop(1000);
                     break;
+                } else if ("update".equalsIgnoreCase(line.trim())){
+                    productList = loadProducts(); 
                 } else {
                     System.out.println("Unknown command. Type 'exit' to stop server gracefully.");
                 }
@@ -137,4 +152,48 @@ public class Main extends WebSocketServer {
             }
         }
     }
+
+    private static JSONArray loadProducts() { //Cargar productos
+        try {
+
+            String userDir = System.getProperty("user.dir");
+            File archivoXML = new File(userDir + "/src/main/resources/data/products.xml");
+            System.out.println(archivoXML.toPath());
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(archivoXML);
+            doc.getDocumentElement().normalize();
+
+            JSONArray productList = new JSONArray();
+
+            System.out.println("Contenido del archivo XML:");
+
+            NodeList listaProductos = doc.getElementsByTagName("producto");
+            for (int i = 0; i < listaProductos.getLength(); i++) {
+                Node nodo = listaProductos.item(i);
+                
+                JSONObject product = new JSONObject();
+                
+                if (nodo.getNodeType() == Node.ELEMENT_NODE) {
+                    Element elemento = (Element) nodo;
+
+                    product.put("id", elemento.getAttribute("id"));
+                    product.put("tags", elemento.getAttribute("tags"));
+                    product.put("nom", elemento.getElementsByTagName("nom").item(0).getTextContent());
+                    product.put("preu", elemento.getElementsByTagName("preu").item(0).getTextContent());
+                    product.put("descripcio", elemento.getElementsByTagName("descripcio").item(0).getTextContent());
+                    product.put("imatge", elemento.getElementsByTagName("imatge").item(0).getTextContent());
+
+                    productList.put(product);
+                }
+            }
+
+            System.out.println(productList);
+
+            return productList;
+
+        } catch (Exception e) {
+            return null;
+        }        
+    }   
 }
