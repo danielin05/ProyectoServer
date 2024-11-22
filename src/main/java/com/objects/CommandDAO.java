4db651b91d822c3dc6fd2a25528700d66279f226
Daniel Artiaga Perez <dartiagaperez.cf@iesesteveterradas.cat>
2024-11-22 18:17:22 +0100

    Funciones actualizarComandas y detalles, actualizar estados y devolver top 5 platos hechas

--- src/main/java/com/objects/CommandDAO.java
+++ src/main/java/com/objects/CommandDAO.java
@@ -5,7 +5,9 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CommandDAO {
 
@@ -17,9 +19,21 @@ public class CommandDAO {
     
         public static void main(String[] args) {
 
+
+            Product pizzaMargherita = new Product("Pizza Margherita", "8.50", "Tomato, mozzarella, and basil", "");
+            pizzaMargherita.addTags(List.of("Italian", "Vegetarian", "caliente"));
+
+            CommandProduct commandPizza1 = new CommandProduct(pizzaMargherita);
+
             Connection conn = DBConnect(DB_URL);
-            //saveNewCommand(conn, addCommand());
-            updateCommand(conn, addCommand());
+
+            // saveNewCommand(conn, addCommand());
+            // updateCommand(conn, addCommand());
+            // updateCommandDetails(conn, addCommand(), commandPizza1);
+            // updateProductStatus(conn, addCommand(), "listo", commandPizza1);
+            // updateCommandStatus(conn, addCommand(), "listo");
+            System.out.println(checkMostSelledProducts(conn));
+
     }
 
     /* FUNCION PARA ESTABLECER LA CONEXION A LA BASE DE DATOS */
@@ -121,14 +135,9 @@ public class CommandDAO {
         String checkCommandId = "SELECT id FROM comanda " + 
                                 "WHERE num_mesa = ? AND estado <> 'pagado';"; 
 
-        String deletePastDetailsCommand = "DELETE FROM detalle_comanda WHERE id_comanda = ? AND estado_producto <> 'pagado';";
-
         String updateCommand = "UPDATE comanda " + 
                                "SET id_camarero = ?, num_mesa = ?, clientes = ?, estado = ?, precio_total = ? " + 
                                "WHERE id = ?;";
-
-        String createDetailsCommand = "INSERT INTO detalle_comanda(id_comanda, nombre_producto, descripcion_producto, precio_producto, estado_producto) " +
-                                      "VALUES(?, ?, ?, ?, ?);";
     
         try (PreparedStatement pstmt = conn.prepareStatement(checkCommandId)) {
 
@@ -161,36 +170,173 @@ public class CommandDAO {
         } catch (NumberFormatException | SQLException e) {
             e.printStackTrace();
         } 
-
-        try (PreparedStatement pstmt = conn.prepareStatement(deletePastDetailsCommand)) {
-            pstmt.setInt(1, commandId); // ID de la comanda
-    
-            pstmt.executeUpdate();
-            System.out.println("SE HA BORRADO CORRECTAMENTE LOS DETALLES ANTIGUOS DE LA COMANDA");
-    
-            } catch (NumberFormatException | SQLException e) {
-                e.printStackTrace();
-            } 
-
-        for (CommandProduct commandProduct : comanda.getProducts()){
-
-            try (PreparedStatement pstmt = conn.prepareStatement(createDetailsCommand)) {
-                pstmt.setInt(1, commandId);                                                      // ID de la comanda
-                pstmt.setString(2, commandProduct.getProducte().getNombre());                   // Nombre del producto
-                pstmt.setString(3, commandProduct.getProducte().getDescription());              // Descripcion del producto
-                pstmt.setDouble(4, Double.parseDouble(commandProduct.getProducte().getPreu())); // Precio del producto
-                pstmt.setString(5, commandProduct.getEstado());                                 // Estado del producto
-
-                pstmt.executeUpdate();
-                System.out.println("SE HAN CREADO CORRECTAMENTE LOS NUEVOS DETALLES DE LA COMANDA");
-
-            } catch (NumberFormatException | SQLException e) {
-                e.printStackTrace();
-            } 
-        }
-    
     }
 
+    public static void updateCommandDetails(Connection conn, Comanda comanda, CommandProduct producto){
+
+        comanda.addProducts(List.of(producto));
+
+        updateCommand(conn, comanda);
+
+        Integer commandId = 0;
+
+        String checkCommandId = "SELECT id FROM comanda " + 
+                                "WHERE num_mesa = ? AND estado <> 'pagado';"; 
+
+        String updateCommandProducts = "INSERT INTO detalle_comanda(id_comanda, nombre_producto, descripcion_producto, precio_producto, estado_producto) " +
+        "VALUES(?, ?, ?, ?, ?);";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(checkCommandId)) {
+
+            pstmt.setInt(1, comanda.getNumber());
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                commandId = rs.getInt("id");
+            }
+
+            rs.close();
+
+        } catch (SQLException e) {
+            System.out.println("Esta comanda no existe " + e.getMessage());
+        }
+
+
+        try (PreparedStatement pstmt = conn.prepareStatement(updateCommandProducts)) {
+            pstmt.setInt(1, commandId);                                               // ID de la comanda
+            pstmt.setString(2, producto.getProducte().getNombre());                   // Nombre del producto
+            pstmt.setString(3, producto.getProducte().getDescription());              // Descripcion del producto
+            pstmt.setDouble(4, Double.parseDouble(producto.getProducte().getPreu())); // Precio del producto
+            pstmt.setString(5, producto.getEstado());                                 // Estado del producto
+
+            pstmt.executeUpdate();
+            System.out.println("SE HAN ACTUALIZADO CORRECTAMENTE LOS DETALLES DE LA COMANDA");
+
+        } catch (NumberFormatException | SQLException e) {
+            e.printStackTrace();
+        }   
+
+    }
+
+    public static void updateCommandStatus(Connection conn, Comanda comanda, String newStatus){
+
+        Integer commandId = 0;
+
+        String checkCommandId = "SELECT id FROM comanda " + 
+                                "WHERE num_mesa = ? AND estado <> 'pagado';"; 
+
+        String updateCommandStatus = "UPDATE comanda " +
+                                     "SET estado = ? " +
+                                     "WHERE id = ?;";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(checkCommandId)) {
+
+            pstmt.setInt(1, comanda.getNumber());
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                commandId = rs.getInt("id");
+            }
+
+            rs.close();
+
+        } catch (SQLException e) {
+            System.out.println("Esta comanda no existe " + e.getMessage());
+        }
+
+        try (PreparedStatement pstmt = conn.prepareStatement(updateCommandStatus)) {
+            pstmt.setString(1, newStatus);
+            pstmt.setInt(2, commandId);
+
+            pstmt.executeUpdate();
+
+            System.out.println("Se ha actualizado el estado de la comanda");
+
+        } catch (SQLException e) {
+            System.out.println("Esta comanda no existe " + e.getMessage());
+        }
+
+    } 
+
+    public static void updateProductStatus(Connection conn, Comanda comanda, String newStatus, CommandProduct producto){
+
+        Integer commandId = 0;
+
+        String checkCommandId = "SELECT id FROM comanda " + 
+                                "WHERE num_mesa = ? AND estado <> 'pagado';"; 
+
+        String updateProductStatus = "UPDATE detalle_comanda " +
+                                     "SET estado_producto = ? " +
+                                     "WHERE id_comanda = ? AND nombre_producto = ? AND descripcion_producto = ? AND precio_producto = ? AND estado_producto = ? " +
+                                     "LIMIT 1;";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(checkCommandId)) {
+
+            pstmt.setInt(1, comanda.getNumber());
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                commandId = rs.getInt("id");
+            }
+
+            rs.close();
+
+        } catch (SQLException e) {
+            System.out.println("Esta comanda no existe " + e.getMessage());
+        }
+
+        try (PreparedStatement pstmt = conn.prepareStatement(updateProductStatus)) {
+
+            pstmt.setString(1, newStatus);
+            pstmt.setInt(2, commandId);                                               // ID de la comanda
+            pstmt.setString(3, producto.getProducte().getNombre());                   // Nombre del producto
+            pstmt.setString(4, producto.getProducte().getDescription());              // Descripcion del producto
+            pstmt.setDouble(5, Double.parseDouble(producto.getProducte().getPreu())); // Precio del producto
+            pstmt.setString(6, producto.getEstado());                                 // Estado del producto
+            
+
+            pstmt.executeUpdate();
+            System.out.println("Estado de producto actualizado correctamente");
+            
+        } catch (SQLException e) {
+            System.out.println("El producto no existe " + e.getMessage());
+        }
+    }
+
+    public static Map<String, Integer> checkMostSelledProducts(Connection conn){
+
+        Map<String, Integer> products = new HashMap<>();
+        String productName = "";
+        Integer productAmount = 0;
+        
+        String checkProducts = "SELECT nombre_producto, COUNT(nombre_producto) AS total_vendidos " + 
+                               "FROM detalle_comanda " +
+                               "GROUP BY nombre_producto " +
+                               "ORDER BY total_vendidos ASC " +
+                               "LIMIT 5;";
+
+        try (PreparedStatement pstmt = conn.prepareStatement(checkProducts)) {
+
+        ResultSet rs = pstmt.executeQuery();
+
+        while (rs.next()) {
+            productName = rs.getString("nombre_producto");
+            productAmount = rs.getInt("total_vendidos");
+
+            products.put(productName, productAmount);
+        }
+
+        rs.close();
+
+    } catch (SQLException e) {
+        System.out.println("Esta comanda no existe " + e.getMessage());
+    }
+
+        return products;
+    }
 
     /* FUNCION TEMPORAL PARA HACER PRUEBAS SE BORRARA MAS ADELANTE */
 
@@ -209,7 +355,7 @@ public class CommandDAO {
         CommandProduct commandPizza3 = new CommandProduct(pizzaMargherita);
         CommandProduct commandSpaghetti3 = new CommandProduct(spaghettiCarbonara);
         
-        Comanda comanda1 = new Comanda(1, 6, new ClientFX("Daniel", "2", "321"));
+        Comanda comanda1 = new Comanda(1, 6, new ClientFX("Pedro", "3", "987"));
         comanda1.addProducts(List.of(commandPizza1, commandSpaghetti1, commandPizza2, commandSpaghetti2, commandPizza3, commandSpaghetti3));
         
         return comanda1;
